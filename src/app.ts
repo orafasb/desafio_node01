3e7d12b19b202ef8bb0329c187e2dcdcae1fe6b3
Rafael Sieben Bordignon <rafaelsbordignon@gmail.com>
2020-12-14 00:39:02 -0300

    'desafio'

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import 'dotenv/config';
 
 import express, { Request, Response, NextFunction } from 'express';
 import 'express-async-errors';
+import cors from 'cors';
 
 import routes from './routes';
 import AppError from './errors/AppError';
@@ -11,24 +12,25 @@ import createConnection from './database';
 
 createConnection();
 const app = express();
+app.use(cors());
 
 app.use(express.json());
 app.use(routes);
 
-app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
-  if (err instanceof AppError) {
-    return response.status(err.statusCode).json({
+app.use(
+  (err: Error, _request: Request, response: Response, _: NextFunction) => {
+    if (err instanceof AppError) {
+      return response.status(err.statusCode).json({
+        status: 'error',
+        message: err.message,
+      });
+    }
+
+    return response.status(500).json({
       status: 'error',
-      message: err.message,
+      message: 'Internal server error',
     });
-  }
-
-  console.error(err);
-
-  return response.status(500).json({
-    status: 'error',
-    message: 'Internal server error',
-  });
-});
+  },
+);
 
 export default app;
